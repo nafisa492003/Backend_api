e00b19e0517c65a202cf062236d56453a5264d12
nafisa492003 <afifayasmin2003@gmail.com>
2025-09-30 04:49:44 +0600

    fixing cors problem

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://e-commerce-fdr.netlify.app/", 
+  origin: "https://e-commerce-fdr.netlify.app", 
   credentials: true
 }));
 
